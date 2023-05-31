b97124bfa4fd0d568f32cc22cf134a9c879f3d34
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-05-31 13:13:35 +0900

    fix: show player2 board

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import { BattleShip } from "./BattleShip";
 const P1 = new Player("Perry", "platypus", false);
 const P2 = new Player("Minochan", "monkey", true);
 const battleship = new BattleShip(P1, P2);
-P1.gameboard.receiveAttack(4,6)
-P1.gameboard.receiveAttack(0,0)
+P1.gameboard.receiveAttack(4, 6);
+P1.gameboard.receiveAttack(0, 0);
+P2.gameboard.receiveAttack(0, 0);
+P2.gameboard.receiveAttack(1, 0);
+P2.gameboard.receiveAttack(3, 0);
+P2.gameboard.receiveAttack(4, 0);
 
 function App() {
   const [counter, setCounter] = useState(0);
@@ -17,31 +21,56 @@ function App() {
   };
 
   const currentGBP1 = battleship.P1.gameboard.currentGameboard();
+  const currentGBP2 = battleship.P2.gameboard.currentGameboard();
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="name">{battleship.P1.name}</div>
         <div className="name">{battleship.P2.name}</div>
+        <div id="gameboard-field">
+          <div id="gameboard-p1">
+            {currentGBP1.map((row, r) => {
+              return (
+                <div className="row">
+                  {row.map((cell, c) => {
+                    if (cell.ship && cell.attacked) {
+                      return <div className="cell">👨🏿‍🦲</div>;
+                    } else if (cell.ship && !cell.attacked) {
+                      return <div className="cell">👨‍🦲</div>;
+                    } else if (!cell.ship && cell.attacked) {
+                      return <div className="cell">💨</div>;
+                    } else {
+                      return <div className="cell">🌱</div>;
+                    }
+                  })}
+                </div>
+              );
+            })}
+          </div>
 
-        <div id="gameboard">
-          {currentGBP1.map((row, r) => {
-            return (
-              <div className="row">
-                {row.map((cell, c) => {
-                  if (cell.ship && cell.attacked) {
-                    return <div className="cell">👨🏿‍🦲</div>;
-                  } else if (cell.ship && !cell.attacked) {
-                    return <div className="cell">👨‍🦲</div>;
-                  }else if (!cell.ship && cell.attacked) {
-                    return <div className="cell">💨</div>;
-                  } else {
-                    return <div className="cell">🌱</div>;
-                  }
-                })}
-              </div>
-            );
-          })}
+          <div id="gameboard-p2">
+            {currentGBP2.map((row, r) => {
+              return (
+                <div className="row">
+                  {row.map((cell, c) => {
+                    console.log(cell.ship);
+                    if (cell.ship?.isSunk()) {
+                      return <div className="cell">👨🏿‍🦲</div>;
+                    }
+
+                    if (cell.ship && cell.attacked) {
+                      return <div className="cell">👨‍🦲</div>;
+                    } else if (!cell.ship && cell.attacked) {
+                      return <div className="cell">💨</div>;
+                    } else {
+                      return <div className="cell">🌱</div>;
+                    }
+                  })}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </header>
     </div>
