ebdbf12dc4a68ae4d528cf7ed2ac36525f09f697
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-06-03 08:32:06 +0900

    fix: place p1 ship by click div without overlay

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import { BattleShip } from "./BattleShip";
 const P1 = new Player("Perry", "platypus", false);
 const P2 = new Player("Minochan", "monkey", true);
 const battleship = new BattleShip(P1, P2);
-P1.gameboard.receiveAttack(4, 6);
-P1.gameboard.receiveAttack(0, 0);
-P2.gameboard.receiveAttack(0, 0);
-P2.gameboard.receiveAttack(1, 0);
-P2.gameboard.receiveAttack(3, 0);
-P2.gameboard.receiveAttack(4, 0);
+// P1.gameboard.receiveAttack(4, 6);
+// P1.gameboard.receiveAttack(0, 0);
+// P2.gameboard.receiveAttack(0, 0);
+// P2.gameboard.receiveAttack(1, 0);
+// P2.gameboard.receiveAttack(3, 0);
+// P2.gameboard.receiveAttack(4, 0);
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [gameStatus, setGameStatus] = useState("not-started");
+  // eslint-disable-next-line
+  const [direction, setDirection] = useState(["x", "y", "x", "y", "x"]);
 
   const updateCounter = function () {
     setCounter(counter + 1);
@@ -27,7 +30,7 @@ function App() {
       console.log("You Win");
     } else {
       const getHit = battleship.P2.doAIMove(battleship.P1.gameboard);
-
+      console.log(getHit, getHit[0], getHit[1]);
       battleship.P1.gameboard.receiveAttack(getHit[0], getHit[1]);
       updateCounter();
     }
@@ -41,55 +44,151 @@ function App() {
       <header className="App-header">
         <div className="name">{battleship.P1.name}</div>
         <div className="name">{battleship.P2.name}</div>
+
+        {gameStatus !== "on-game" ? (
+          <button
+            id="get-ready-btn"
+            onClick={() => {
+              battleship.P1.getReady(battleship);
+              setGameStatus("on-game");
+              updateCounter();
+            }}
+          >
+            START
+          </button>
+        ) : null}
+
+        {gameStatus === "on-game" ? (
+          <button
+            id="start-btn"
+            onClick={() => {
+              // battleship.P1.junbi(battleship);
+              setGameStatus("not-started");
+              updateCounter();
+            }}
+          >
+            Restart
+          </button>
+        ) : null}
+
         <div id="gameboard-field">
-          <div id="gameboard-p1">
-            {currentGBP1.map((row, y) => {
-              return (
-                <div className="row">
+          {gameStatus === "not-started" ? (
+            <div id="gameboard-preparation">
+              {currentGBP1.map((row, y) => (
+                <div className="row" key={y}>
                   {row.map((cell, x) => {
-                    if (cell.ship && cell.attacked) {
-                      return <div className="cell">👨🏿‍🦲</div>;
-                    } else if (cell.ship && !cell.attacked) {
-                      return <div className="cell">👨‍🦲</div>;
-                    } else if (!cell.ship && cell.attacked) {
-                      return <div className="cell">💨</div>;
-                    } else {
-                      return <div className="cell">🌱</div>;
-                    }
-                  })}
-                </div>
-              );
-            })}
-          </div>
-
-          <div id="gameboard-p2">
-            {currentGBP2.map((row, y) => {
-              return (
-                <div className="row">
-                  {row.map((cell, x) => {
-                    if (cell.ship?.isSunk()) {
-                      return <div className="cell">👨🏿‍🦲</div>;
-                    }
-
-                    if (cell.ship && cell.attacked) {
-                      return <div className="cell">👨‍🦲</div>;
-                    } else if (!cell.ship && cell.attacked) {
-                      return <div className="cell">💨</div>;
+                    if (cell.ship) {
+                      return (
+                        <div className="cell" key={`${x}${y}`}>
+                          🐒
+                        </div>
+                      );
                     } else {
                       return (
                         <div
                           className="cell"
-                          onClick={() => updateOnAttack(x, y)}
+                          key={`${x}${y}`}
+                          onClick={() => {
+                            battleship.P1.placeShip(
+                              battleship,
+                              x,
+                              y,
+                              direction
+                            );
+                            updateCounter();
+                          }}
                         >
-                          🌱
+                          -
                         </div>
                       );
                     }
                   })}
                 </div>
-              );
-            })}
-          </div>
+              ))}
+            </div>
+          ) : null}
+
+          {gameStatus === "on-game" ? (
+            <div>
+              <div id="gameboard-p1">
+                {currentGBP1.map((row, y) => {
+                  return (
+                    <div className="row">
+                      {row.map((cell, x) => {
+                        if (cell.ship && cell.attacked) {
+                          return (
+                            <div className="cell" key={`${x}${y}`}>
+                              👨🏿‍🦲
+                            </div>
+                          );
+                        } else if (cell.ship && !cell.attacked) {
+                          return (
+                            <div className="cell" key={`${x}${y}`}>
+                              🐒
+                            </div>
+                          );
+                        } else if (!cell.ship && cell.attacked) {
+                          return (
+                            <div className="cell" key={`${x}${y}`}>
+                              ✷
+                            </div>
+                          );
+                        } else {
+                          return (
+                            <div className="cell" key={`${x}${y}`}>
+                              🌱
+                            </div>
+                          );
+                        }
+                      })}
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div id="gameboard-p2">
+                {currentGBP2.map((row, y) => {
+                  return (
+                    <div className="row">
+                      {row.map((cell, x) => {
+                        if (cell.ship?.isSunk()) {
+                          return (
+                            <div className="cell" key={`p2${x}${y}`}>
+                              👨🏿‍🦲
+                            </div>
+                          );
+                        }
+
+                        if (cell.ship && cell.attacked) {
+                          return (
+                            <div className="cell" key={`p2${x}${y}`}>
+                              ❗️
+                            </div>
+                          );
+                        } else if (!cell.ship && cell.attacked) {
+                          return (
+                            <div className="cell" key={`p2${x}${y}`}>
+                              ✷
+                            </div>
+                          );
+                        } else {
+                          return (
+                            <div
+                              className="cell"
+                              onClick={() => updateOnAttack(x, y)}
+                              key={`p2${x}${y}`}
+                            >
+                              🌱
+                            </div>
+                          );
+                        }
+                      })}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          ) : null}
         </div>
       </header>
     </div>
--- src/Player.js
+++ src/Player.js
@@ -7,26 +7,6 @@ export class Player {
     this.character = character;
     this.isAI = isAI;
     this.gameboard = new Gameboard();
-
-    const ship1 = new Ship(2);
-    ship1.setCoordinates(0, 0, "x");
-    this.gameboard.addShip(ship1);
-
-    const ship2 = new Ship(3);
-    ship2.setCoordinates(3, 0, "y");
-    this.gameboard.addShip(ship2);
-
-    const ship3 = new Ship(3);
-    ship3.setCoordinates(4, 3, "x");
-    this.gameboard.addShip(ship3);
-
-    const ship4 = new Ship(4);
-    ship4.setCoordinates(1, 3, "y");
-    this.gameboard.addShip(ship4);
-
-    const ship5 = new Ship(5);
-    ship5.setCoordinates(2, 9, "x");
-    this.gameboard.addShip(ship5);
   }
 
   doAIMove(p1gameboard) {
@@ -40,5 +20,138 @@ export class Player {
     }
     const random = Math.floor(Math.random() * (remainingCells.length - 1));
     return remainingCells[random];
+  }
+
+  placeShip(battleship, x, y, direction) {
+    const getShipLength = function () {
+      const shipArrayLength = battleship.P1.gameboard.ships.length;
+      if (shipArrayLength === 0) {
+        return 2;
+      } else if (shipArrayLength === 1) {
+        return 3;
+      } else if (shipArrayLength === 2) {
+        return 3;
+      } else if (shipArrayLength === 3) {
+        return 4;
+      } else if (shipArrayLength === 4) {
+        return 5;
+      } else {
+        return;
+      }
+    };
+
+    // you cannot place a ship if there's another ship
+    // you cannot place a ship if it exceeds the board
+
+    let isOutOfBoard = false;
+    const coordinatesToBeSet = [];
+
+    const shipArrayLength = battleship.P1.gameboard.ships.length;
+    console.log(shipArrayLength, direction[shipArrayLength]);
+    if (direction[shipArrayLength] === "x") {
+      for (let i = x; i < getShipLength() + x; i++) {
+        coordinatesToBeSet.push([i, y]);
+        if (i > Gameboard.size) {
+          console.log(Gameboard.size);
+          isOutOfBoard = true;
+        }
+      }
+    }
+
+    if (direction[shipArrayLength] === "y") {
+      for (let i = y; i < getShipLength() + y; i++) {
+        coordinatesToBeSet.push([x, i]);
+        if (i > Gameboard.size) {
+          console.log(Gameboard.size);
+          isOutOfBoard = true;
+        }
+      }
+    }
+    console.log("coordinatesToBeSet", coordinatesToBeSet);
+    console.log("p1 ships coordinates", battleship.P1.gameboard.ships);
+
+    let isTaken = false;
+
+    if (battleship.P1.gameboard.ships.length !== 0) {
+      isTaken = coordinatesToBeSet.some((cooA) => {
+        return battleship.P1.gameboard.ships.some((ship) => {
+          return ship.coordinates.some((cooB) => {
+            return cooA[0] === cooB[0] && cooA[1] === cooB[1];
+          });
+        });
+      });
+    }
+    if (isTaken || isOutOfBoard) {
+      // do nothing
+      return;
+    } else {
+      const ship1 = new Ship(2);
+      const ship2 = new Ship(3);
+      const ship3 = new Ship(3);
+      const ship4 = new Ship(4);
+      const ship5 = new Ship(5);
+
+      // add ship
+      if (battleship.P1.gameboard.ships.length === 0) {
+        ship1.setCoordinates(x, y, direction[0]);
+        battleship.P1.gameboard.addShip(ship1);
+      } else if (battleship.P1.gameboard.ships.length === 1) {
+        ship2.setCoordinates(x, y, direction[1]);
+        battleship.P1.gameboard.addShip(ship2);
+      } else if (battleship.P1.gameboard.ships.length === 2) {
+        ship3.setCoordinates(x, y, direction[2]);
+        battleship.P1.gameboard.addShip(ship3);
+      } else if (battleship.P1.gameboard.ships.length === 3) {
+        ship4.setCoordinates(x, y, direction[3]);
+        battleship.P1.gameboard.addShip(ship4);
+      } else if (battleship.P1.gameboard.ships.length === 4) {
+        ship5.setCoordinates(x, y, direction[4]);
+        battleship.P1.gameboard.addShip(ship5);
+      }
+
+      console.log(battleship.P1.gameboard.ships);
+    }
+  }
+
+  getReady(battleship) {
+    console.log("Prepare");
+
+    // const ship1 = new Ship(2);
+    // const ship2 = new Ship(3);
+    // const ship3 = new Ship(3);
+    // const ship4 = new Ship(4);
+    // const ship5 = new Ship(5);
+
+    // ship2.setCoordinates(3, 0, "y");
+    // battleship.P1.gameboard.addShip(ship2);
+    // ship3.setCoordinates(4, 3, "x");
+    // battleship.P1.gameboard.addShip(ship3);
+    // ship4.setCoordinates(1, 3, "y");
+    // battleship.P1.gameboard.addShip(ship4);
+    // ship5.setCoordinates(2, 9, "x");
+    // battleship.P1.gameboard.addShip(ship5);
+
+    // P2
+    // const p2ship1 = new Ship(2);
+    // p2ship1.setCoordinates(0, 0, "x");
+    // battleship.P2.gameboard.addShip(p2ship1);
+
+    // const p2ship2 = new Ship(3);
+    // p2ship2.setCoordinates(3, 0, "y");
+    // battleship.P2.gameboard.addShip(p2ship2);
+
+    // const p2ship3 = new Ship(3);
+    // p2ship3.setCoordinates(4, 3, "x");
+    // battleship.P2.gameboard.addShip(p2ship3);
+
+    // const p2ship4 = new Ship(4);
+    // p2ship4.setCoordinates(1, 3, "y");
+    // battleship.P2.gameboard.addShip(p2ship4);
+
+    // const p2ship5 = new Ship(5);
+    // p2ship5.setCoordinates(2, 9, "x");
+    // battleship.P2.gameboard.addShip(p2ship5);
+
+    console.log(battleship.P1.gameboard);
   }
 }
